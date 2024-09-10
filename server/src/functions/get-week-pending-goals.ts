007159ea716bec9376655325a7e8013c8407ccd8
Gonçalo Fonseca <goncalofonseca2003.g@gmail.com>
2024-09-10 17:45:00 +0100

    end of day1

---
+++ server/src/functions/get-week-pending-goals.ts
@@ -0,0 +1,60 @@
+import dayjs from 'dayjs'
+import weekOfYear from 'dayjs/plugin/weekOfYear'
+import { db } from '../db'
+import { goalCompletions, goals } from '../db/schema'
+import { and, lte, sql, count, gte, eq } from 'drizzle-orm'
+
+dayjs.extend(weekOfYear)
+
+export async function getWeekPendingGoals() {
+  const firstDayOfWeek = dayjs().startOf('week').toDate()
+  const lastDayOfWeek = dayjs().endOf('week').toDate()
+
+  const goalsCreateUpToWeek = db.$with('goals_create_up_to_week').as(
+    db
+      .select({
+        id: goals.id,
+        title: goals.title,
+        desiredWeeklyFrequency: goals.desiredWeeklyFrequency,
+        createdAt: goals.createdAt,
+      })
+      .from(goals)
+      .where(lte(goals.createdAt, lastDayOfWeek))
+  )
+
+  const goalCompletionCounts = db.$with('goal_completion_counts').as(
+    db
+      .select({
+        goalId: goalCompletions.goalId,
+        completionCount: count(goalCompletions.id).as('completionCount'),
+      })
+      .from(goalCompletions)
+      .where(
+        and(
+          gte(goalCompletions.createdAt, firstDayOfWeek),
+          lte(goalCompletions.createdAt, lastDayOfWeek)
+        )
+      )
+      .groupBy(goalCompletions.goalId)
+  )
+
+  const pendignGoals = await db
+    .with(goalsCreateUpToWeek, goalCompletionCounts)
+    .select({
+      id: goalsCreateUpToWeek.id,
+      title: goalsCreateUpToWeek.title,
+      desiredWeeklyFrequency: goalsCreateUpToWeek.desiredWeeklyFrequency,
+      completionCount: sql`
+      COALESCE(${goalCompletionCounts.completionCount}, 0)
+      `.mapWith(Number),
+    })
+    .from(goalsCreateUpToWeek)
+    .leftJoin(
+      goalCompletionCounts,
+      eq(goalCompletionCounts.goalId, goalsCreateUpToWeek.id)
+    )
+
+  return {
+    pendignGoals,
+  }
+}
